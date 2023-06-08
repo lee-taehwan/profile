a422e40903a272f3b3fc6f113b025a4d7d1ecb9c
lee-taehwan <soslth@naver.com>
2023-06-08 20:01:10 +0900

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -589,37 +589,6 @@
               <h5 class="font-weight-bold m-0">Client Name</h5>
               <span>Profession</span>
             </div>
-            <div class="text-center">
-              <i class="fa fa-3x fa-quote-left text-primary mb-4"></i>
-              <h4 class="font-weight-light mb-4">
-                Dolor eirmod diam stet kasd sed. Aliqu rebum est eos. Rebum
-                elitr dolore et eos labore, stet justo sed est sed. Diam sed sed
-                dolor stet accusam amet eirmod eos, labore diam clita
-              </h4>
-              <img
-                class="img-fluid rounded-circle mx-auto mb-3"
-                src="@/assets/images/testimonial-1.jpg"
-                style="width: 80px; height: 80px"
-                alt="`${../assets/images/testimonial-1.jpg}`"
-              />
-              <h5 class="font-weight-bold m-0">Client Name</h5>
-              <span>Profession</span>
-            </div>
-            <div class="text-center">
-              <i class="fa fa-3x fa-quote-left text-primary mb-4"></i>
-              <h4 class="font-weight-light mb-4">
-                Dolor eirmod diam stet kasd sed. Aliqu rebum est eos. Rebum
-                elitr dolore et eos labore, stet justo sed est sed. Diam sed sed
-                dolor stet accusam amet eirmod eos, labore diam clita
-              </h4>
-              <img
-                class="img-fluid rounded-circle mx-auto mb-3"
-                src="../assets/images/testimonial-3.jpg"
-                style="width: 80px; height: 80px"
-              />
-              <h5 class="font-weight-bold m-0">Client Name</h5>
-              <span>Profession</span>
-            </div>
           </div>
         </div>
       </div>
